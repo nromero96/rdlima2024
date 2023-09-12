3c2f960c4d0906f1e27fe2b4769c11829f3cb47d
nromero96 <niltondeveloper96@gmail.com>
2023-09-12 16:59:14 -0500

    Actualizacion de formulario de trabajos

--- public/assets/js/apps/works/create.js
+++ public/assets/js/apps/works/create.js
@@ -40,3 +40,27 @@ textarea.addEventListener("input", updateCharCount);
 // Llamar a la función al cargar la página
 updateCharCount();
 
+
+// Obtener el elemento select y el textarea
+const selectCategory = document.getElementById("inputCategory");
+const textareaDescription = document.getElementById("inputDescription");
+
+// Agregar un evento de cambio al select
+selectCategory.addEventListener("change", function () {
+    // Obtener el valor seleccionado
+    const selectedCategory = selectCategory.value;
+
+    // Cambiar el contenido del textarea según la categoría seleccionada
+    switch (selectedCategory) {
+        case "Dermatólogo Joven":
+        case "Trabajo de Investigación Científica":
+            textareaDescription.value = "FUNDAMENTOS\n\nOBJETIVO\n\nMETODOS\n\nRESULTADOS\n\nCONCLUSION";
+            break;
+        case "Mini Caso":
+            textareaDescription.value = "FUNDAMENTOS\n\nMOTIVO DE LA COMUNICACIÓN\n\nRELATO DEL CASO\n\nDISCUCIÓN";
+            break;
+        default:
+            textareaDescription.value = ""; // Limpiar el textarea si no se selecciona nada
+            break;
+    }
+});
