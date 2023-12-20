b0ce9a01bcfb18ffc1ed1b24ef63e21adb1a5f7a
nromero96 <niltondeveloper96@gmail.com>
2023-12-20 10:14:12 -0500

    Se agregaron nuevas columnas a inscripciones

---
+++ database/migrations/2023_12_20_100706_add_comprobantes_columns_to_inscriptions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddComprobantesColumnsToInscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inscriptions', function (Blueprint $table) {
+            $table->string('status_compr')->default('Pendiente');
+            $table->string('type_compr')->nullable();
+            $table->string('num_compr')->nullable();
+            $table->string('compr_pdf')->nullable();
+            $table->string('compr_xml')->nullable();
+            $table->string('compr_cvr')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inscriptions', function (Blueprint $table) {
+            //
+        });
+    }
+}
