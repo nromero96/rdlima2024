594437263b7a48c2e3f8e33b78aa262ae77ae4a2
nromero96 <niltondeveloper96@gmail.com>
2024-04-03 18:40:11 -0500

    nombre del post no se muestra en alguno

--- resources/views/pages/posters/index.blade.php
+++ resources/views/pages/posters/index.blade.php
@@ -97,7 +97,7 @@
                                                         
                                                         @if($item->poster_file)
                                                             <a href="{{ asset('storage/uploads/poster_files/' . $item->poster_file) }}" target="_blank" class="text-secondary" title="{{ $item->poster_file }}">
-                                                                {{ strlen($item->poster_file) > 18 ? substr($item->poster_file, 0, 18) . '...' : $item->poster_file }}
+                                                                {{ strlen($item->poster_file) > 30 ? substr($item->poster_file, 0, 30) . '...' : $item->poster_file }}
                                                             </a>
 
                                                             @if(\Auth::user()->hasRole('Administrador') || \Auth::user()->hasRole('Secretaria'))
